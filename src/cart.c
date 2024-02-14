efa145e770e9cb7682df683df6135417ab63788a
Daniel Coady <me@pondo.dev>
2024-02-15 09:03:28 +1100

    parse ines format info

--- src/cart.c
+++ src/cart.c
@@ -7,6 +7,9 @@
 #include <errno.h>
 #include <stdint.h>
 
+static inline int _parse_ines(Cart* cart);
+static inline int _parse_ines20(Cart* cart);
+
 int cart_load(const char* path, Cart* cart) {
     TraceLog(LOG_INFO, "loading cart from path '%s'...", path);
 
@@ -38,6 +41,26 @@ int cart_load(const char* path, Cart* cart) {
         goto bail;
     }
 
+    const int ines_format = cart->buffer[0] == 'N' &&
+                            cart->buffer[1] == 'E' &&
+                            cart->buffer[2] == 'S' &&
+                            cart->buffer[3] == 26; // MS-DOS EOF
+    if (ines_format) {
+        const int ines20_format = (cart->buffer[7] & 0x0C) == 0x08;
+        if (ines20_format) {
+            success = _parse_ines20(cart);
+        } else {
+            success = _parse_ines(cart);
+        }
+
+        if (! success)
+            goto bail;
+    } else {
+        TraceLog(LOG_ERROR, "failed to load card (unrecognised ROM format)");
+        success = 0;
+        goto bail;
+    }
+
     TraceLog(LOG_INFO, "done!");
 
 bail:
@@ -46,8 +69,37 @@ bail:
 }
 
 void cart_unload(Cart* cart) {
-    cart->size = 0;
     free(cart->buffer);
-    cart->buffer = NULL;
+    memset(cart, 0, sizeof(*cart));
+}
+
+static inline int _parse_ines(Cart* cart) {
+    TraceLog(LOG_INFO, "ROM is iNES format");
+    cart->type = kROMTYPE_INES;
+
+    cart->prg_rom_size = cart->buffer[4]*16*1000;
+    cart->chr_rom_size = cart->buffer[5]*8*1000;
+    cart->prg_ram_size = cart->buffer[8]*8*1000;
+
+    cart->nametable_arrangement = cart->buffer[6] & (1 << 0) ? kNAMETABLE_HORIZONTAL : kNAMETABLE_VERTICAL;
+
+    cart->mapper = (cart->buffer[6] & 0xF0) >> 4 | (cart->buffer[7] & 0xF0);
+
+    TraceLog(LOG_INFO, "ROM info:");
+    TraceLog(LOG_INFO, "PRG ROM size (bytes): %zu", cart->prg_rom_size);
+    TraceLog(LOG_INFO, "CHR ROM size (bytes): %zu", cart->chr_rom_size);
+    TraceLog(LOG_INFO, "PRG RAM size (bytes): %zu", cart->prg_ram_size);
+    TraceLog(LOG_INFO, "nametable arrangement: %s", cart->nametable_arrangement == kNAMETABLE_VERTICAL ? "vertical" : "horizontal");
+    TraceLog(LOG_INFO, "mapper: %u", cart->mapper);
+
+    return 1;
+}
+
+static inline int _parse_ines20(Cart* cart) {
+    TraceLog(LOG_INFO, "ROM is iNES 2.0 format");
+    cart->type = kROMTYPE_INES20;
+
+    TraceLog(LOG_ERROR, "iNES 2.0 parsing not implemented!");
+    return 0;
 }
 
--- src/cart.h
+++ src/cart.h
@@ -3,9 +3,26 @@
 
 #include <stdlib.h>
 
+typedef enum {
+    kROMTYPE_NONE = 0,
+    kROMTYPE_INES,
+    kROMTYPE_INES20,
+} ROMType;
+
+typedef enum {
+    kNAMETABLE_VERTICAL,
+    kNAMETABLE_HORIZONTAL,
+} NametableArrangement;
+
 typedef struct {
-    size_t      size;
-    uint8_t*    buffer;
+    ROMType                 type;
+    size_t                  size;
+    uint8_t*                buffer;
+    size_t                  prg_rom_size;
+    size_t                  chr_rom_size;
+    size_t                  prg_ram_size;
+    NametableArrangement    nametable_arrangement;
+    uint16_t                mapper;
 } Cart;
 
 int cart_load(const char* path, Cart* cart);
