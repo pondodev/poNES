5626a3ff8a8fdaa25bcea8ab148eb7718d0a42d0
Daniel Coady <me@pondo.dev>
2024-08-20 10:16:40 +1000

    fix startup values

--- src/device.c
+++ src/device.c
@@ -6,16 +6,26 @@
 
 #include <string.h>
 
+typedef enum {
+    kSTATUSFLAG_CARRY       = 1 << 0,
+    kSTATUSFLAG_ZERO        = 1 << 1,
+    kSTATUSFLAG_IRQ_DISABLE = 1 << 2,
+    kSTATUSFLAG_DECIMAL     = 1 << 3,
+    kSTATUSFLAG_OVERFLOW    = 1 << 6,
+    kSTATUSFLAG_NEGATIVE    = 1 << 6,
+} StatusFlag;
+
 Device g_device;
 
 void device_init(Cart* cart) {
+    // initial values based on https://www.nesdev.org/wiki/CPU_power_up_state
     g_device = (Device) {
-        .pc     = cart->prg_rom_start,
-        .sp     = 0xFF, // TODO: where does the sp init to?
+        .pc     = 0xFFFC,
+        .sp     = 0xFD,
         .acc    = 0x00,
         .x      = 0x00,
         .y      = 0x00,
-        .status = 0x00,
+        .status = 0x00 & kSTATUSFLAG_IRQ_DISABLE,
         .cart   = cart,
     };
 
